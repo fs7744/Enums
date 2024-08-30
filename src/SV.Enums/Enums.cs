775a306bf57485ec90f2764e07c1f471179a6128
Victor Qu <Victor.X.Qu@newegg.com>
2024-08-30 16:29:00 +0800

    ToEnum

--- src/SV.Enums/Enums.cs
+++ src/SV.Enums/Enums.cs
@@ -991,6 +991,72 @@ namespace SV
                     throw new InvalidOperationException();
             }
         }
+
+        public static T ToEnum(string value)
+        {
+            if (TryParse(value, out T result))
+                return result;
+
+            switch (GetUnderlyingTypeCode())
+            {
+                case TypeCode.Int32:
+                    {
+                        var v = Convert.ToInt32(value);
+                        return Unsafe.As<Int32, T>(ref v);
+                    }
+
+                case TypeCode.Byte:
+                    {
+                        var v = Convert.ToByte(value);
+                        return Unsafe.As<Byte, T>(ref v);
+                    }
+
+                case TypeCode.Char:
+                    {
+                        var v = Convert.ToChar(value);
+                        return Unsafe.As<Char, T>(ref v);
+                    }
+
+                case TypeCode.Int16:
+                    {
+                        var v = Convert.ToInt16(value);
+                        return Unsafe.As<Int16, T>(ref v);
+                    }
+
+                case TypeCode.Int64:
+                    {
+                        var v = Convert.ToInt64(value);
+                        return Unsafe.As<Int64, T>(ref v);
+                    }
+
+                case TypeCode.SByte:
+                    {
+                        var v = Convert.ToSByte(value);
+                        return Unsafe.As<SByte, T>(ref v);
+                    }
+
+                case TypeCode.UInt16:
+                    {
+                        var v = Convert.ToUInt16(value);
+                        return Unsafe.As<UInt16, T>(ref v);
+                    }
+
+                case TypeCode.UInt32:
+                    {
+                        var v = Convert.ToUInt32(value);
+                        return Unsafe.As<UInt32, T>(ref v);
+                    }
+
+                case TypeCode.UInt64:
+                    {
+                        var v = Convert.ToUInt64(value);
+                        return Unsafe.As<UInt64, T>(ref v);
+                    }
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
     }
 
     public class EnumInfo<T> : IEnumInfo<T> where T : struct, Enum
